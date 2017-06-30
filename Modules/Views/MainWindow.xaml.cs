6baafb103d6c1d3526ae03bae08d60d7c353a61e
DureSameen <dmeena_pk@yahoo.com>
2017-06-30 05:41:59 +0500

    First Commit

---
+++ Modules/Views/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using System.Windows;
+
+namespace Modules.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IModuleManager _moduleManager;
+        IRegionManager _regionManager;
+        IRegion _region;
+        IUnityContainer _container;
+        public MainWindow(IModuleManager moduleManager, IRegionManager regionManager, IUnityContainer  container)
+        {
+            InitializeComponent();
+            _moduleManager = moduleManager;
+            _regionManager = regionManager;
+            _container = container;
+            var catalog = (ModuleCatalog)_container.Resolve<IModuleCatalog>();
+ 
+             foreach (var module in catalog.Modules )
+                 _moduleManager.LoadModule(module.ModuleName);
+             
+        }
+
+
+       
+      
+
+      
+    }
+}
