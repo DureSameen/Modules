be9194c911a7bfcdaad5d2a83bd522e91549d63c
duresameen <dotnetpearl@gmail.com>
2017-07-06 15:33:25 +0500

    merge

--- SCClientApp.SetupDialogue/ApplicationModules.cs
+++ SCClientApp.SetupDialogue/ApplicationModules.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace SCClientApp.SetupDialogue
 {
@@ -22,10 +23,15 @@ namespace SCClientApp.SetupDialogue
 
             try
             {
+<<<<<<< HEAD
 
 
 
                 string serverwebapi_url = "http://localhost:60375/api/customer/edition_url/";
+=======
+ 
+                 string serverwebapi_url =  loadConfig( working_dir_path); //ConfigurationManager.AppSettings["serverwebapi_url"]; // "http://localhost/SCServerApi/api/customer/edition_url/";
+>>>>>>> d8888daca4d49fc917bd5d5607b405e208307f4c
                 client.BaseAddress = new Uri(serverwebapi_url);
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -64,7 +70,16 @@ namespace SCClientApp.SetupDialogue
             { }
         }
 
+        private static string loadConfig(string working_dir_path)
+        {
 
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(working_dir_path + "app.config");
+            XmlNode xnodes = xdoc.SelectSingleNode("/configuration/appSettings");
+            return xnodes.ChildNodes[0].Attributes[1].Value;
+             
+
+        }
 
     }
 }
